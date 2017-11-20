11fb6372d9264bb64ffb6664390113f8951b3b21
Guilherme lucioni <guil_2011@hotmail.com.br>
2017-11-20 17:03:00 -0200

    Grafico não numérico

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -19,7 +19,12 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+        public ActionResult Grafico()
+        {
+            ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
